755bd0acba1c2fa55fc34566bc793b6b1abc15a6
kevinrisqi <kevinrisqi22@gmail.com>
2022-07-14 15:46:05 +0700

    add custom sort

--- bin/sort/main.dart
+++ bin/sort/main.dart
@@ -10,11 +10,28 @@ void main(List<String> args) {
     Person(name: 'Dita', age: 22),
     Person(name: 'Javier', age: 33),
     Person(name: 'Vera', age: 25),
+    Person(name: 'Zahra', age: 25),
+    Person(name: 'Dina', age: 25),
+    Person(name: 'Dono', age: 17),
+    Person(name: 'Dono', age: 16),
   ];
 
   int counter = 0;
 
-  persons.sort(((a, b) => a.age - b.age));
+  // sort by name
+  // persons.sort(((a, b) => a.name!.compareTo(b.name!)));
+
+  // sort by age
+  // persons.sort(((a, b) => a.age - b.age));
+
+  // double sort by name & age
+  persons.sort(((a, b) {
+    if (a.name!.compareTo(b.name!) != 0) {
+      return a.name!.compareTo(b.name!);
+    } else {
+      return a.age.compareTo(b.age);
+    }
+  }));
 
   persons.forEach((element) {
     if (counter == 0) {
